4d014739d73a029a2fe8551ac6876abac6c900dc
MassiveBattlebotsFan <teamlightning2018@outlook.com>
2023-11-03 13:02:34 -0500

    Add project files.

---
+++ Assembly-CSharp.Motor_Loader.mm/ComponentWrapperClasses.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using UnityEngine;
+using MonoMod;
+using Mono.Cecil;
+
+
+[Serializable]
+public class JSON_Part_Data
+{
+    [ComponentName]
+    public string name;
+    [TextArea(5, 5)]
+    public string description;
+    public string string_ID;
+    public float weight;
+    public Vector3 reference_position;
+    public Vector3 center_of_mass;
+    public CompType comp_type = 0;  
+}
+
+[Serializable]
+public class JSON_Motor_Data : JSON_Part_Data
+{
+    public float max_voltage;
+    public float kV_rating;
+    public float internal_resistance;
+    public float max_cont_current;
+    public float max_rotor_rpm;
+    public float max_rpm;
+    public float max_safe_torque;
+    public float stall_torque;
+    public bool is_brushless;
+    
+    public JSON_Motor_Data()
+    {
+        this.comp_type = CompType.SpinMotor;
+    }
+    public JSON_Motor_Data(Comp_Info_Motor construct_from)
+    {
+        this.comp_type = CompType.SpinMotor;
+        if (construct_from == null) return;
+        this.max_voltage = construct_from.max_volts_before_fried;
+        this.max_rpm = construct_from.max_rpm;
+        this.max_rotor_rpm = construct_from.max_rotor_RPM;
+        this.description = construct_from.description;
+        this.name = construct_from.comp_name;
+        this.stall_torque = construct_from.stall_torque;
+        this.max_safe_torque = construct_from.max_safe_torque;
+        this.internal_resistance = construct_from.internal_resistance;
+        this.is_brushless = construct_from.is_brushless;
+        this.weight = construct_from.weight;
+        this.max_cont_current = construct_from.max_continuous_current;
+        this.center_of_mass = construct_from.localCOM;
+        this.reference_position = construct_from.referencePosition;
+        this.string_ID = construct_from.comp_string_id;
+        this.kV_rating = construct_from.kV_rating;
+    }
+    public Comp_Info_Motor GetCompInfo()
+    {
+        Comp_Info_Motor temp = new Comp_Info_Motor();
+        temp.max_volts_before_fried = this.max_voltage;
+        temp.max_rpm = this.max_rpm;
+        temp.max_rotor_RPM = this.max_rotor_rpm;
+        temp.description = this.description;
+        temp.comp_name = this.name;
+        temp.stall_torque = this.stall_torque;
+        temp.max_safe_torque = this.max_safe_torque;
+        temp.internal_resistance = this.internal_resistance;
+        temp.is_brushless = this.is_brushless;
+        temp.weight = this.weight;
+        temp.max_continuous_current = this.max_cont_current;
+        temp.localCOM = this.center_of_mass;
+        temp.referencePosition = this.reference_position;
+        temp.comp_type = this.comp_type;
+        temp.comp_string_id = this.string_ID;
+        temp.kV_rating = this.kV_rating;
+        temp.comp_set = 2;
+        return temp;
+    }
+}
---
+++ Assembly-CSharp.Motor_Loader.mm/ResourcesPatch.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using UnityEngine;
+using MonoMod;
+using Mono.Cecil;
+
+
+[Serializable]
+public class DataDumpClass
+{
+    [SerializeField]
+    public List<Mesh_Wrapper> meshes = new List<Mesh_Wrapper>();
+    [SerializeField]
+    public Component_Info compinfo;
+    [SerializeField]
+    public string stringid;
+}
+
+[Serializable]
+public class patch_Component_Info : Component_Info
+{
+
+}
+
+public class patch_Robot_Resources : Robot_Resources
+{
+    [MonoModIgnore]
+    private static bool filled;
+
+    private static bool dumped = false;
+
+    public static extern void orig_fillResources();
+
+    public static void dumpParts()
+    {
+        if (!dumped)
+        {
+            Directory.CreateDirectory(GlobalDirectories.RobotDirectory.FullName);
+            foreach (var i in patch_Robot_Resources.std_comp_list)
+            {
+                DataDumpClass data = new DataDumpClass();
+                data.stringid = i.Key;
+                data.compinfo = new Component_Info();
+                data.compinfo = i.Value.GetComponent<Component_Info>();
+                Debug.Log("adding meshes");
+                foreach (var j in i.Value.transform.GetComponentsInChildren<MeshFilter>())
+                {
+                    data.meshes.Add(new Mesh_Wrapper(j.mesh));
+                }
+                if(data.compinfo.comp_type == CompType.SpinMotor)
+                {
+                    JSON_Motor_Data temp = new JSON_Motor_Data(i.Value.GetComponent<Comp_Info_Motor>());
+                    Debug.Log("CompType.SpinMotor, dumping motorinfo wrapper class");
+                    File.WriteAllText(GlobalDirectories.RobotDirectory.FullName + new string(i.Key.Where(m => !System.IO.Path.GetInvalidFileNameChars().Contains(m)).ToArray<char>()) + "-motorinfo.json", JsonUtility.ToJson(temp, true));
+                    File.WriteAllText(GlobalDirectories.RobotDirectory.FullName + new string(i.Key.Where(m => !System.IO.Path.GetInvalidFileNameChars().Contains(m)).ToArray<char>()) + "-compinfo2.json", JsonUtility.ToJson(temp.GetCompInfo(), true));
+                }
+                //data[i.Key]["comp_info"] = JsonUtility.ToJson(i.Value.GetComponent<Component_Info>(), true);
+                //File.WriteAllText(GlobalDirectories.RobotDirectory.FullName + new string(i.Key.Where(m => !System.IO.Path.GetInvalidFileNameChars().Contains(m)).ToArray<char>()) + "-compinfo.json", JsonUtility.ToJson(i.Value.GetComponent<Component_Info>(), true));
+                //File.WriteAllText(GlobalDirectories.RobotDirectory.FullName + new string(i.Key.Where(m => !System.IO.Path.GetInvalidFileNameChars().Contains(m)).ToArray<char>()) + ".json", JsonUtility.ToJson(data, true));
+            }
+            Debug.Log(GlobalDirectories.RobotDirectory.FullName);
+            //Debug.Log(temp.Length);
+            //File.WriteAllText(GlobalDirectories.RobotDirectory.FullName + "componentList.json", temp);
+            Debug.Log("Dumped components");
+            dumped = true;
+        }
+    }
+
+    public static void fillResources()
+    {
+        orig_fillResources();
+        dumpParts();
+
+    }
+}
